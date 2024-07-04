ede33b7c5aa1c87e202486a5efad83eb6fc4c5e0
abdelfetah18 <nstart0319@gmail.com>
2024-07-04 19:33:45 +0100

    Replace UnixSocket with WebSocket for code-execution service

--- app/code_execution/CodeExecutionService.ts
+++ app/code_execution/CodeExecutionService.ts
@@ -1,10 +1,10 @@
-import net from 'net';
+import { WebSocket, RawData } from 'ws';
 import Queue from '../services/QueueService';
 
 const EXECUTION_SERVICE_PORT = process.env.EXECUTION_SERVICE_PORT ? parseInt(process.env.EXECUTION_SERVICE_PORT) : 3001;
 
 export default class CodeExecutionService {
-    client: net.Socket;
+    client: WebSocket;
     clientId: string;
     payload: ExecutionPayload;
     queue: Queue;
@@ -14,16 +14,17 @@ export default class CodeExecutionService {
         this.payload = payload;
         this.queue = queue;
 
-        this.client = net.createConnection({ port: EXECUTION_SERVICE_PORT }, () => {
-            console.log('Connected to server at ', EXECUTION_SERVICE_PORT);
-        });
+        this.client = new WebSocket(`ws://localhost:${EXECUTION_SERVICE_PORT}`);
+        this.client.onopen = () => {
+            console.log('Connected to code-execution service at ', EXECUTION_SERVICE_PORT);
+        }
 
-        this.client.on('data', this.onReceive);
-        this.client.on('end', this.onEnd);
+        this.client.on('message', this.onReceive);
+        this.client.on('close', this.onEnd);
         this.client.on('error', (err) => { console.log('CodeExecutionService', err); });
     }
 
-    onReceive = (data: Buffer): void => {
+    onReceive = (data: RawData): void => {
         try {
             // console.log('Received:', data.toString());
             const executionResult = JSON.parse(data.toString()) as ExecutionResult;
@@ -43,8 +44,10 @@ export default class CodeExecutionService {
         console.log('Disconnected from server');
     }
 
-    send = (request: ExecutionRequest) => {
+    send = async (request: ExecutionRequest) => {
         // console.log({ request });
-        this.client.write(JSON.stringify(request));
+        const promise = new Promise((resolve, reject) => { this.client.onopen = () => { resolve(true); } });
+        await promise;
+        this.client.send(JSON.stringify(request));
     }
 }
